185b801d5c893d4791ba7c7148b2cfec5f135bd2
Nemil <noora.lennartz@metropolia.fi>
2023-03-15 13:53:26 +0200

    pug

--- week1-node/app.js
+++ week1-node/app.js
@@ -2,10 +2,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  res.send('index')
 })
+app.set('view engine', 'pug')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
