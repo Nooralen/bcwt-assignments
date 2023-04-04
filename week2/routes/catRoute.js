f44ed5365b71d859acd1d58a3f03dff238944130
Nemil <noora.lennartz@metropolia.fi>
2023-04-04 14:43:31 +0300

    week3

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -22,9 +22,43 @@ router.get('/', controller.getCatList);
 //Tietty kissa
 router.get('/:id', controller.getCat);
 // POST
-router.post('/', upload.single('cat'), controller.cat_create_post, controller.postCat);
+router.post('/', 
+upload.single('cat'), 
+body('name').isAlpha().isLength({min: 3}).withMessage('Name must be atleast 3 characters').trim().escape(),
+body('birthdate').isISO8601().withMessage('Invalid birthdate'),
+body('weight').isNumeric().withMessage('Weight must be a number'),
+body('owner').notEmpty().withMessage('Owner is required'),
+body('cat').custom((value, {req}) => {
+  if (!req.file) {
+    throw new Error('File is required');
+
+  }else if (!['image/png', 'image/jpeg', 'image/gif'].includes(req.file.mimetype)) {
+    throw new Error ('Invalid file type');
+  }else {
+    return true;
+  }
+}),
+(req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array() });
+
+  }
+  controller.cat_create_post(req,res);
+});
 // PUT
-router.put('/', controller.cat_update_put)
+router.put('/', body('name').isAlpha().isLength({min: 3}).withMessage('Name must be atleast 3 characters').trim().escape(),
+body('birthdate').isISO8601().withMessage('Invalid birthdate'),
+body('weight').isNumeric().withMessage('Weight must be a number'),
+body('owner').notEmpty().withMessage('Owner is required'),
+(req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array() });
+  }
+  controller.cat_update_put(req, res);
+
+}); 
 // DELETE
 router.delete('/:id', controller.cat_delete);
 module.exports = router
--- week2/routes/userRoute.js
+++ week2/routes/userRoute.js
@@ -8,8 +8,17 @@ router.get('/', controller.getUserList);
 //Tietty käyttäjä
 router.get('/:id', controller.getUser);
 // POST
-router.post('/', controller.user_create_post, controller.postUser);
-
+router.post('/', body('name').isAlpha().isLength({ min: 3 }).withMessage('Name must be at least 3 characters').trim().escape(),
+  body("passwd").isLength({ min: 8 }).withMessage('Name must be at least 8 characters').trim().escape(),
+  body("email").isEmail().withMessage('Email must be valid').isEmail().normalizeEmail(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    controller.user_create_post(req, res);
+    controller.postUser(req, res);
+  });
 // PUT
 router.put('/', (req, res) => {
   res.send("With this endpoint you can edit users.")
